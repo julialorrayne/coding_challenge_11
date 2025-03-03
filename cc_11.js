ade9938f84a0f246a6b46a38b52dd3ff2e7e9beb
Julia <lorrayne@usf.edu>
2025-03-02 22:33:17 -0500

    Task 4 - Implemented Book Borrowing

--- cc_11.js
+++ cc_11.js
@@ -14,15 +14,6 @@ class Book {     //// Created a class Book with properties title, author, isbn,c
     }
 };
 
-//test:
-const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
-console.log(book1.getDetails());
-// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
-
-book1.updateCopies(-1);
-console.log(book1.getDetails());
-// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
-
 //Task 2: Creating a Borrower Class
 class Borrower {  //Created a class Borrower 
     constructor(name, borrowerId) {
@@ -41,17 +32,6 @@ class Borrower {  //Created a class Borrower
     }
 };
 
-//test:
-const borrower1 = new Borrower("Alice Johnson", 201);
-borrower1.borrowBook("The Great Gatsby");
-console.log(borrower1.borrowedBook);
-// Expected output: ["The Great Gatsby"]
-
-borrower1.returnBook("The Great Gatsby");
-console.log(borrower1.borrowedBook);
-// Expected output: []
-
-
 //Task 3: Creating a Library Class
 class Library {  //Created a class Library
     constructor(){
@@ -64,7 +44,48 @@ class Library {  //Created a class Library
     listBooks(){  //method listBooks(): Logs all books' details
         this.books.forEach(book => console.log(book.getDetails()));
     }
+    lendBook(borrowerId,isbn){ //Task 4: Implementing Book Borrowing
+        const book = this.books.find(book => book.isbn ===isbn);
+        const borrower = this.borrowers.find(borrower => borrower.borrowerId === borrowerId);
+        if(!book){
+            console.log(`The book with isbn ${isbn} was not found`);
+            return;
+        }  //Added a method lendBook(borrowerId, isbn) in the Library class
+        if(!borrower){
+            console.log(`The borrower with id ${borrowerId} was not found`);
+            return;
+        }
+        if (book.copies > 0) {
+            book.updateCopies(-1);
+            borrower.borrowBook(book.title);
+            console.log(`The book ${book.title} was borrowed by ${borrower.name}`);
+        }
+        else {
+            console.log(`Unfortunately, the book ${book.title} is not available`)
+        }
+    }
 };
+
+//test:
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBook);
+// Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBook);
+// Expected output: []
+
+
+//test:
+const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
+
+book1.updateCopies(-1);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
 
 //test:
 const library = new Library();
@@ -73,4 +94,9 @@ library.listBooks();
 // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
 
 
-
+//test:
+library.lendBook(201, 123456);
+console.log(book1.getDetails());
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 3"
+console.log(borrower1.borrowedBook);
+// Expected output: ["The Great Gatsby"]
